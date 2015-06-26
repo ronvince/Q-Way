fc57b1cb922cd032d365e6c8af0af928486b2e25
user <user@users-Mac-Pro-9.local>
2015-06-26 15:06:15 +0530

    Main Project

---
+++ IndoorNavigationApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndoorNavigationApp
+//
+//  Created by user on 6/26/15.
+//  Copyright (c) 2015 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IndoorNavigationApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IndoorNavigationApp
+//
+//  Created by user on 6/26/15.
+//  Copyright (c) 2015 user. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
